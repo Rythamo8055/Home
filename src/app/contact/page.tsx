87662288fa4d4a4a55e0db08bf99c5877007824d
Vishnu <mailforservices8055@gmail.com>
2025-06-18 12:51:12 +0000

    i am getting scroll bar i dont need scrool bar remove that

--- src/app/contact/page.tsx
+++ src/app/contact/page.tsx
@@ -1,3 +1,4 @@
+
 import { Header } from "@/components/common/Header";
 import { Footer } from "@/components/common/Footer";
 import { ContactForm } from "@/components/contact/ContactForm";
@@ -5,7 +6,7 @@ import { ChatbotFab } from "@/components/chatbot/ChatbotFab";
 
 export default function ContactPage() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col flex-1">
       <Header />
       <main className="flex-grow container py-12 md:py-16 lg:py-20 flex items-center justify-center">
         <ContactForm />
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,3 +1,4 @@
+
 import { Header } from "@/components/common/Header";
 import { Footer } from "@/components/common/Footer";
 import { AppShowcase } from "@/components/apps/AppShowcase";
@@ -5,7 +6,7 @@ import { ChatbotFab } from "@/components/chatbot/ChatbotFab";
 
 export default function HomePage() {
   return (
-    <div className="flex flex-col min-h-screen">
+    <div className="flex flex-col flex-1">
       <Header />
       <main className="flex-grow">
         <AppShowcase />
